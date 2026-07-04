62ee199db83c77a13741789c280c46bc2f77f83a
Manthan Siroya <237607330+ManthanSiroya@users.noreply.github.com>
2026-07-05 03:24:37 +0530

    Sync Codeforces: 2241A - Divide and Conquer [C++17 (GCC 7-32)]

--- 2241A_Divide_and_Conquer.cpp
+++ 2241A_Divide_and_Conquer.cpp
@@ -13,29 +13,33 @@ int main()
     {
         ll x,y, i;
         cin >> x>>y;
-        vector<ll> v;
-        v.push_back(1);
-        v.push_back(x);
-        for ( i = 2; i*i <= x; i++)
-        {
-            if(x%i==0){
-                v.push_back(i);
- 
-                if(i!=(x/i))
-                    v.push_back(x/i);
-            }
-        }
-        ll ans=0;
-        for(auto xx:v){
-            if(xx==y){
-                ans=1;
-                break;
-            }
-        }
-        if(ans)
+        if(x%y==0)
         cout<<"YES\n";
         else
         cout<<"NO\n";
+        // vector<ll> v;
+        // v.push_back(1);
+        // v.push_back(x);
+        // for ( i = 2; i*i <= x; i++)
+        // {
+        //     if(x%i==0){
+        //         v.push_back(i);
+ 
+        //         if(i!=(x/i))
+        //             v.push_back(x/i);
+        //     }
+        // }
+        // ll ans=0;
+        // for(auto xx:v){
+        //     if(xx==y){
+        //         ans=1;
+        //         break;
+        //     }
+        // }
+        // if(ans)
+        // cout<<"YES\n";
+        // else
+        // cout<<"NO\n";
  
     }
     return 0;
