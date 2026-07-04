1bddb5f17e2c8722b3d9fbd1e91d7d67d5a436dd
Manthan Siroya <237607330+ManthanSiroya@users.noreply.github.com>
2026-07-05 03:37:21 +0530

    Sync Codeforces: 133A - HQ9+ [C++20 (GCC 13-64)]

---
+++ 133A_HQ9.cpp
@@ -0,0 +1,54 @@
+// Winners never Quit!
+#include <bits/stdc++.h> // Problem - 
+using namespace std;
+ 
+#define ll long long
+#define pr pair<ll, ll>
+#define pb push_back
+#define fr(i, a, n) for (ll i = a; i < n; i++)
+#define frr(i, a, n) for (ll i = n - 1; i >= a; i--)
+#define elif else if
+ 
+bool isprime(ll n)
+{
+    if (n < 2) return false;
+    for (ll i = 2; i * i <= n; i++)
+        if (n % i == 0) return false;
+    return true;
+}
+ 
+ll pow(ll a, ll b) // O(log(b))
+{ 
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1) res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+void solve()
+{
+    string s;
+    cin>>s;
+    fr(i,0,(s.size())){
+        if(s[i]=='H'||s[i]=='Q'||s[i]=='9'){
+            cout<<"YES\n";
+            return;
+        }
+    }
+    cout<<"NO\n";
+    return;
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    // ll T; cin >> T;
+    // while (T--)
+    solve();
+    return 0;
+}
