4c7c2caf22535c2b8d3f238399b213d7ac84c6a5
Manthan Siroya <237607330+ManthanSiroya@users.noreply.github.com>
2026-07-05 03:24:56 +0530

    Sync Codeforces: 2240C - Nim Game Is XOR Game [C++17 (GCC 7-32)]

--- 2240C_Nim_Game_Is_XOR_Game.cpp
+++ 2240C_Nim_Game_Is_XOR_Game.cpp
@@ -35,7 +35,7 @@ int main()
         ll ans = 0;
         for (i = 0; i < n; i++)
         {
-            if ((xora ^ v[i]) < v[i])
+            if ((xora ^ v[i]) <= v[i])
                 ans++;
         }
         cout << ans % 998244353 << endl;
