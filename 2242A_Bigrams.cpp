e3fb03bce08dde3d131175dbf7c5cf63990be03d
Manthan Siroya <237607330+ManthanSiroya@users.noreply.github.com>
2026-07-06 20:16:20 +0530

    Sync Codeforces: 2242A - Bigrams [C++20 (GCC 13-64)]

---
+++ 2242A_Bigrams.cpp
@@ -0,0 +1,63 @@
+// Winners never Quit!
+#include <bits/stdc++.h> // Problem - 
+using namespace std;
+ 
+#define ll long long
+#define pr pair<ll, ll>
+#define pb push_back
+#define fr(i, a, n) for (ll i = a; i < n; i++)
+#define frr(i, a, n) for (ll i = n - 1; i >= a; i--)
+#define elif else if
+ 
+bool isprime(ll n)
+{
+    if (n < 2) return false;
+    for (ll i = 2; i * i <= n; i++)
+        if (n % i == 0) return false;
+    return true;
+}
+ 
+ll pow(ll a, ll b) // O(log(b))
+{ 
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1) res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+void solve()
+{
+    ll n;
+    cin>>n;
+    vector<ll> v(n);
+    map<ll,ll> freq;
+    fr(i,0,n) {
+        cin>>v[i];
+        freq[v[i]]++;
+    }
+    ll ans=0;
+    if(freq[2]>1)
+        ans=1;
+    fr(i,0,n){
+        if(v[i]>=3){
+            ans=1;
+            break;
+        }
+    }
+    if(ans) cout<<"YES\n";
+    else cout<<"NO\n";
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    ll T; cin >> T;
+    while (T--)
+    solve();
+    return 0;
+}
