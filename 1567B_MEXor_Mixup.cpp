6b1a62ef582c92c2509e1a6cc7eff8d1e5a4338a
Manthan Siroya <237607330+ManthanSiroya@users.noreply.github.com>
2026-07-11 22:29:31 +0530

    Sync Codeforces: 1567B - MEXor Mixup [C++20 (GCC 13-64)]

---
+++ 1567B_MEXor_Mixup.cpp
@@ -0,0 +1,56 @@
+// Winners never Quit!
+#include <bits/stdc++.h> // Problem - 
+using namespace std;
+ 
+#define ll long long
+#define pr pair<ll, ll>
+#define pb push_back
+#define fr(i, a, n) for (ll i = a; i < n; i++)
+#define frr(i, a, n) for (ll i = n - 1; i >= a; i--)
+#define elif else if
+ 
+bool isprime(ll n)
+{
+    if (n < 2) return false;
+    for (ll i = 2; i * i <= n; i++)
+        if (n % i == 0) return false;
+    return true;
+}
+ 
+ll pow(ll a, ll b) // O(log(b))
+{ 
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1) res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+void solve()
+{
+    ll a,b,x=0;
+    cin>>a>>b;
+    ll ans=a+1;
+ 
+    if(a%4==0) x=0;
+    elif(a%4==1) x=a-1;
+    elif(a%4==2) x=1;
+    else x=a;
+ 
+    if((x^b)==a) ans++;
+    if(x==b) ans--;
+    cout<<ans<<endl;
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    ll T; cin >> T;
+    while (T--)
+    solve();
+    return 0;
+}
