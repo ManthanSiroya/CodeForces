8c44b1d9bf9a0abfa508dcdb0ab2daea6bdc4310
Manthan Siroya <237607330+ManthanSiroya@users.noreply.github.com>
2026-07-15 02:18:48 +0530

    Sync Codeforces: 2244C - Stepan and Permutation [C++20 (GCC 13-64)]

---
+++ 2244C_Stepan_and_Permutation.cpp
@@ -0,0 +1,60 @@
+// Winners never Quit!
+#include <bits/stdc++.h> // Problem - 
+using namespace std;
+ 
+#define ll long long
+#define pr pair<ll, ll>
+#define pb push_back
+#define fr(i, a, n) for (ll i = a; i < n; i++)
+#define frr(i, a, n) for (ll i = n - 1; i >= a; i--)
+#define elif else if
+ 
+bool isprime(ll n)
+{
+    if (n < 2) return false;
+    for (ll i = 2; i * i <= n; i++)
+        if (n % i == 0) return false;
+    return true;
+}
+ 
+ll pow(ll a, ll b) // O(log(b))
+{ 
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1) res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+void solve()
+{
+    ll n,x,y;
+    cin>>n>>x>>y;
+    vector<pair<ll,ll>> v(n);
+    fr(i,0,n) {
+        cin>>v[i].first;
+        v[i].second=i;
+    }
+    ranges::sort(v);
+    ll cnt=gcd(x,y);
+    fr(i,0,n){
+        if((v[i].second-i)%cnt){
+            cout<<"NO\n";
+            return;
+        }
+    }
+    cout<<"YES\n";
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    ll T; cin >> T;
+    while (T--)
+    solve();
+    return 0;
+}
