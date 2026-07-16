a79f5cedf1fc5e5b55f35e57e7525dc62b30dae1
Manthan Siroya <237607330+ManthanSiroya@users.noreply.github.com>
2026-07-16 22:48:31 +0530

    Sync Codeforces: 2245B - Delete and Concatenate [C++20 (GCC 13-64)]

---
+++ 2245B_Delete_and_Concatenate.cpp
@@ -0,0 +1,58 @@
+// Winners never Quit!
+#include <bits/stdc++.h> // Problem - 
+using namespace std;
+ 
+#define ll long long
+#define pr pair<ll, ll>
+#define pb push_back
+#define fr(i, a, n) for (ll i = a; i < n; i++)
+#define frr(i, a, n) for (ll i = n - 1; i >= a; i--)
+#define elif else if
+ 
+bool isprime(ll n)
+{
+    if (n < 2) return false;
+    for (ll i = 2; i * i <= n; i++)
+        if (n % i == 0) return false;
+    return true;
+}
+ 
+ll pow(ll a, ll b) // O(log(b))
+{ 
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1) res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+void solve()
+{
+    ll n,c;
+    cin>>n>>c;
+    vector<ll> v(n);
+    ll ans=0;
+    fr(i,0,n) {
+        cin>>v[i];
+        ans+=(v[i]-c);
+    }
+    ranges::sort(v);
+    fr(i,0,(n/2)){
+        if(v[i]<c) ans-=(v[i]-c);
+        else break;
+    }
+    cout<<ans<<endl;
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    ll T; cin >> T;
+    while (T--)
+    solve();
+    return 0;
+}
