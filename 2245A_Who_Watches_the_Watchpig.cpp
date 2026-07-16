7ddde997c375ccf4d3315c3030aa624fcb1407e0
Manthan Siroya <237607330+ManthanSiroya@users.noreply.github.com>
2026-07-16 20:54:23 +0530

    Sync Codeforces: 2245A - Who Watches the Watchpig? [C++20 (GCC 13-64)]

---
+++ 2245A_Who_Watches_the_Watchpig.cpp
@@ -0,0 +1,59 @@
+// Winners never Quit!
+#include <bits/stdc++.h> // Problem - 
+using namespace std;
+ 
+#define ll long long
+#define pr pair<ll, ll>
+#define pb push_back
+#define fr(i, a, n) for (ll i = a; i < n; i++)
+#define frr(i, a, n) for (ll i = n - 1; i >= a; i--)
+#define elif else if
+ 
+bool isprime(ll n)
+{
+    if (n < 2) return false;
+    for (ll i = 2; i * i <= n; i++)
+        if (n % i == 0) return false;
+    return true;
+}
+ 
+ll pow(ll a, ll b) // O(log(b))
+{ 
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1) res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+void solve()
+{
+    ll n,k;
+    cin>>n>>k;
+    string s; cin>>s;
+    if((2*k)>n){
+        cout<<"-1\n";
+        return;
+    }
+    ll cnt1=0,cnt2=0;
+    fr(i,0,k){
+        if(s[i]!='R') cnt1++;
+    }
+    frr(i,n-k,n){
+        if(s[i]!='L') cnt2++;
+    }
+    cout<<(cnt1+cnt2)<<endl;
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    ll T; cin >> T;
+    while (T--)
+    solve();
+    return 0;
+}
