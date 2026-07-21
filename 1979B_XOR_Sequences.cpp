606e3316b3b3ec935f9fd10f3f36d2ac9f374806
Manthan Siroya <237607330+ManthanSiroya@users.noreply.github.com>
2026-07-21 23:58:20 +0530

    Sync Codeforces: 1979B - XOR Sequences [C++20 (GCC 13-64)]

---
+++ 1979B_XOR_Sequences.cpp
@@ -0,0 +1,53 @@
+// Winners never Quit!
+#include <bits/stdc++.h> // Problem - 
+using namespace std;
+ 
+#define ll long long
+#define pr pair<ll, ll>
+#define pb push_back
+#define fr(i, a, n) for (ll i = a; i < n; i++)
+#define frr(i, a, n) for (ll i = n - 1; i >= a; i--)
+#define elif else if
+ 
+bool isprime(ll n)
+{
+    if (n < 2) return false;
+    for (ll i = 2; i * i <= n; i++)
+        if (n % i == 0) return false;
+    return true;
+}
+ 
+ll pow(ll a, ll b) // O(log(b))
+{ 
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1) res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+void solve()
+{
+    ll x,y;
+    cin>>x>>y;
+    ll d = x ^ y;
+    ll ans = 1;
+    while ((d & 1) == 0) {
+        ans <<= 1;
+        d >>= 1;
+    }
+    cout << ans << '\n';
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    ll T; cin >> T;
+    while (T--)
+    solve();
+    return 0;
+}
