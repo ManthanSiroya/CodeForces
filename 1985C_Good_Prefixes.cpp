9323092172029b440a176ce089ecaf3587de339f
Manthan Siroya <237607330+ManthanSiroya@users.noreply.github.com>
2026-07-22 23:58:21 +0530

    Sync Codeforces: 1985C - Good Prefixes [C++20 (GCC 13-64)]

---
+++ 1985C_Good_Prefixes.cpp
@@ -0,0 +1,55 @@
+// Winners never Quit!
+#include <bits/stdc++.h> // Problem - 
+using namespace std;
+ 
+#define ll long long
+#define pr pair<ll, ll>
+#define pb push_back
+#define fr(i, a, n) for (ll i = a; i < n; i++)
+#define frr(i, a, n) for (ll i = n - 1; i >= a; i--)
+#define elif else if
+ 
+bool isprime(ll n)
+{
+    if (n < 2) return false;
+    for (ll i = 2; i * i <= n; i++)
+        if (n % i == 0) return false;
+    return true;
+}
+ 
+ll pow(ll a, ll b) // O(log(b))
+{ 
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1) res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+void solve()
+{
+    ll n;
+    cin>>n;
+    vector<ll> v(n);
+    ll sum=0,ans=0,m=0;
+    fr(i,0,n) cin>>v[i];
+    fr(i,0,n){
+        sum+=v[i];
+        m=max(m,v[i]);
+        if(m==(sum-m)) ans++;
+    }
+    cout<<ans<<endl;
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    ll T; cin >> T;
+    while (T--)
+    solve();
+    return 0;
+}
