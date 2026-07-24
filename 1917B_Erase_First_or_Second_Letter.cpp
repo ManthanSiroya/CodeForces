a83ca132bc01791af32d849b82c7d55c2480695b
Manthan Siroya <237607330+ManthanSiroya@users.noreply.github.com>
2026-07-24 22:48:20 +0530

    Sync Codeforces: 1917B - Erase First or Second Letter [C++20 (GCC 13-64)]

---
+++ 1917B_Erase_First_or_Second_Letter.cpp
@@ -0,0 +1,59 @@
+// Winners never Quit!
+#include <bits/stdc++.h> // Problem - 
+using namespace std;
+ 
+#define ll long long
+#define pr pair<ll, ll>
+#define pb push_back
+#define fr(i, a, n) for (ll i = a; i < n; i++)
+#define frr(i, a, n) for (ll i = n - 1; i >= a; i--)
+#define elif else if
+ 
+bool isprime(ll n)
+{
+    if (n < 2) return false;
+    for (ll i = 2; i * i <= n; i++)
+        if (n % i == 0) return false;
+    return true;
+}
+ 
+ll pow(ll a, ll b) // O(log(b))
+{ 
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1) res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+void solve()
+{
+    ll n;
+    cin>>n;
+    string s; cin>>s;
+    map<ll,ll> freq;
+    vector<ll> dp(n,0);
+    freq[s[0]]++;
+    dp[0]=1;
+    fr(i,1,n){
+        freq[s[i]]++;
+        if(freq[s[i]]==1) dp[i] = dp[i-1] + 1;
+        else dp[i] = dp[i-1];
+    }
+    ll ans=0;
+    fr(i,0,n) ans+=dp[i];
+    cout<<ans<<endl;
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    ll T; cin >> T;
+    while (T--)
+    solve();
+    return 0;
+}
