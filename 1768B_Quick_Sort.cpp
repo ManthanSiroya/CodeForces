d175c043bf6d96bd794c3bcf88e835ea392e430b
Manthan Siroya <237607330+ManthanSiroya@users.noreply.github.com>
2026-07-30 23:55:21 +0530

    Sync Codeforces: 1768B - Quick Sort [C++20 (GCC 13-64)]

---
+++ 1768B_Quick_Sort.cpp
@@ -0,0 +1,55 @@
+// Winners never Quit!
+#include <bits/stdc++.h> // Problem - 
+using namespace std;
+ 
+#define ll long long
+#define pr pair<ll, ll>
+#define pb push_back
+#define ran(a) a.begin(), a.end()
+#define fr(i, a, n) for (ll i = a; i < n; i++)
+#define frr(i, a, n) for (ll i = n - 1; i >= a; i--)
+#define elif else if
+ 
+bool isprime(ll n)
+{
+    if (n < 2) return false;
+    for (ll i = 2; i * i <= n; i++)
+        if (n % i == 0) return false;
+    return true;
+}
+ 
+ll pow(ll a, ll b) // O(log(b))
+{ 
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1) res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+void solve()
+{
+    ll n,k;
+    cin>>n>>k;
+    vector<ll> v(n+1);
+    fr(i,1,n+1) {
+        ll x; cin>>x;
+        v[x]=i;
+    }
+    ll ans=1;
+    while (ans<n && v[ans]<v[ans+1]) ans++;
+    cout<<(n-ans+k-1)/k<<"\n";
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    ll T; cin >> T;
+    while (T--)
+    solve();
+    return 0;
+}
