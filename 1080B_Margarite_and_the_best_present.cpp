253c5c80a1ee44fe2498d2d6ba4d389ab064086e
Manthan Siroya <237607330+ManthanSiroya@users.noreply.github.com>
2026-08-03 23:36:21 +0530

    Sync Codeforces: 1080B - Margarite and the best present [C++20 (GCC 13-64)]

---
+++ 1080B_Margarite_and_the_best_present.cpp
@@ -0,0 +1,54 @@
+// Winners never Quit!
+#include <bits/stdc++.h> // Problem - 
+using namespace std;
+ 
+#define ll long long
+#define pr pair<ll, ll>
+#define pb push_back
+#define ran(a) a.begin(), a.end()
+#define fr(i, a, n) for (ll i = a; i < n; i++)
+#define frr(i, a, n) for (ll i = n - 1; i >= a; i--)
+#define elif else if
+ 
+bool isprime(ll n)
+{
+    if (n < 2) return false;
+    for (ll i = 2; i * i <= n; i++)
+        if (n % i == 0) return false;
+    return true;
+}
+ 
+ll pow(ll a, ll b) // O(log(b))
+{ 
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1) res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+void solve()
+{
+    ll x,y;
+    cin>>x>>y;
+    ll ans;
+    if(y%2) ans=(-1)*((y+1)/2);
+    else ans=y/2;
+    if(x%2) ans-=((x-1)/2);
+    else ans-=(-1)*(x/2);
+ 
+    cout<<ans<<endl;
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    ll T; cin >> T;
+    while (T--)
+    solve();
+    return 0;
+}
