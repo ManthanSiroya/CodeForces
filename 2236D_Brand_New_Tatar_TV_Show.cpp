1ce636cd3960517081cdaa7b95af00220b97bda2
Manthan Siroya <237607330+ManthanSiroya@users.noreply.github.com>
2026-08-06 01:04:28 +0530

    Sync Codeforces: 2236D - Brand New Tatar TV Show [C++20 (GCC 13-64)]

---
+++ 2236D_Brand_New_Tatar_TV_Show.cpp
@@ -0,0 +1,67 @@
+// Winners never Quit!
+#include <bits/stdc++.h> // Problem - 
+using namespace std;
+ 
+#define ll long long
+#define pr pair<ll, ll>
+#define pb push_back
+#define ran(a) a.begin(), a.end()
+#define fr(i, a, n) for (ll i = a; i < n; i++)
+#define frr(i, a, n) for (ll i = n - 1; i >= a; i--)
+#define elif else if
+ 
+bool isprime(ll n)
+{
+    if (n < 2) return false;
+    for (ll i = 2; i * i <= n; i++)
+        if (n % i == 0) return false;
+    return true;
+}
+ 
+ll pow(ll a, ll b) // O(log(b))
+{ 
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1) res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+void solve()
+{
+    ll n,k;
+    cin>>n>>k;
+    vector<ll> v(n);
+    map<ll,ll> freq;
+    fr(i,0,n) {
+        cin>>v[i];
+        freq[v[i]]++;
+    }
+    ll ans=0;
+    sort(ran(v),greater<ll>());
+    fr(i,0,n){
+        if(freq[v[i]]%2==0) {
+            ans=1;
+            break;
+        }
+        elif((i+freq[v[i]]<n) && (v[i+freq[v[i]]]>=v[i]-k)){
+            ans=1;
+        }
+        i+=freq[v[i]]-1;
+    }
+    if(ans) cout<<"YES\n";
+    else cout<<"NO\n";
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    ll T; cin >> T;
+    while (T--)
+    solve();
+    return 0;
+}
