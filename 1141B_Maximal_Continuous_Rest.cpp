c9696206d443d0ca9e051c73ce8a57250f10ca68
Manthan Siroya <237607330+ManthanSiroya@users.noreply.github.com>
2026-08-10 22:42:21 +0530

    Sync Codeforces: 1141B - Maximal Continuous Rest [C++20 (GCC 13-64)]

---
+++ 1141B_Maximal_Continuous_Rest.cpp
@@ -0,0 +1,59 @@
+// Winners never Quit!
+#include <bits/stdc++.h> // Problem - 
+using namespace std;
+ 
+#define ll long long
+#define pr pair<ll, ll>
+#define pb push_back
+#define ran(a) a.begin(), a.end()
+#define fr(i, a, n) for (ll i = a; i < n; i++)
+#define frr(i, a, n) for (ll i = n - 1; i >= a; i--)
+#define elif else if
+ 
+bool isprime(ll n)
+{
+    if (n < 2) return false;
+    for (ll i = 2; i * i <= n; i++)
+        if (n % i == 0) return false;
+    return true;
+}
+ 
+ll pow(ll a, ll b) // O(log(b))
+{ 
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1) res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+void solve()
+{
+    ll n;
+    cin>>n;
+    vector<ll> a(n);
+    fr(i,0,n) cin>>a[i];
+    ll ans=0,cnt=0;
+    fr(i,0,2*n) {
+        if (a[i%n]==1) {
+            cnt++;
+            ans=max(ans,cnt);
+        } 
+        else cnt=0;
+    }
+    ans=min(ans,n-1);
+    cout<<ans<<'\n';
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    // ll T; cin >> T;
+    // while (T--)
+    solve();
+    return 0;
+}
