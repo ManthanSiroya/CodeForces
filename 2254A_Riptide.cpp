5ffbda0f6daafb8d5575c3435c544693402bb1af
Manthan Siroya <237607330+ManthanSiroya@users.noreply.github.com>
2026-08-16 23:42:21 +0530

    Sync Codeforces: 2254A - Riptide [C++20 (GCC 13-64)]

---
+++ 2254A_Riptide.cpp
@@ -0,0 +1,49 @@
+// Winners never Quit!
+#include <bits/stdc++.h> // Problem - 
+using namespace std;
+ 
+#define ll long long
+#define pr pair<ll, ll>
+#define pb push_back
+#define ran(a) a.begin(), a.end()
+#define fr(i, a, n) for (ll i = a; i < n; i++)
+#define frr(i, a, n) for (ll i = n - 1; i >= a; i--)
+#define elif else if
+ 
+bool isprime(ll n)
+{
+    if (n < 2) return false;
+    for (ll i = 2; i * i <= n; i++)
+        if (n % i == 0) return false;
+    return true;
+}
+ 
+ll pow(ll a, ll b) // O(log(b))
+{ 
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1) res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+void solve()
+{
+    vector<ll> v(3);
+    fr(i,0,3) cin>>v[i];
+    sort(ran(v));
+    cout<<min(v[1]-v[0],v[2]-v[1])<<'\n';
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    ll T; cin >> T;
+    while (T--)
+    solve();
+    return 0;
+}
