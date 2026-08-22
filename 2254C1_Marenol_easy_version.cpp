5e3401ea607df99e975ae01e8f6cf7fca5defa2a
Manthan Siroya <237607330+ManthanSiroya@users.noreply.github.com>
2026-08-22 23:53:22 +0530

    Sync Codeforces: 2254C1 - Marenol (easy version) [C++20 (GCC 13-64)]

---
+++ 2254C1_Marenol_easy_version.cpp
@@ -0,0 +1,61 @@
+// Winners never Quit!
+#include <bits/stdc++.h> // Problem - 
+using namespace std;
+ 
+#define ll long long
+#define pr pair<ll, ll>
+#define pb push_back
+#define ran(a) a.begin(), a.end()
+#define fr(i, a, n) for (ll i = a; i < n; i++)
+#define frr(i, a, n) for (ll i = n - 1; i >= a; i--)
+#define elif else if
+ 
+bool isprime(ll n)
+{
+    if (n < 2) return false;
+    for (ll i = 2; i * i <= n; i++)
+        if (n % i == 0) return false;
+    return true;
+}
+ 
+ll pow(ll a, ll b) // O(log(b))
+{ 
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1) res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+void solve()
+{
+    ll n;
+    cin>>n;
+    string a,b; cin>>a>>b;
+    ll x=0,y=0,z=0,w=0;
+    fr(i,0,n){
+        if(i%2){
+            if(a[i]=='1') x++;
+            if(b[i]=='1') y++;
+        }
+        else{
+            if(a[i]=='1') z++;
+            if(b[i]=='1') w++;
+        }
+    }
+    if(x==y && z==w) cout<<"YES\n";
+    else cout<<"NO\n";
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    ll T; cin >> T;
+    while (T--)
+    solve();
+    return 0;
+}
