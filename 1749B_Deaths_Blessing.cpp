dff8a1905b7b89c7f67b06b6f39257da3a8f16ff
Manthan Siroya <237607330+ManthanSiroya@users.noreply.github.com>
2026-08-24 00:09:20 +0530

    Sync Codeforces: 1749B - Death's Blessing [C++20 (GCC 13-64)]

---
+++ 1749B_Deaths_Blessing.cpp
@@ -0,0 +1,64 @@
+// Winners never Quit!
+#include <bits/stdc++.h> // Problem - 
+using namespace std;
+ 
+#define ll long long
+#define pr pair<ll, ll>
+#define pb push_back
+#define ran(a) a.begin(), a.end()
+#define fr(i, a, n) for (ll i = a; i < n; i++)
+#define frr(i, a, n) for (ll i = n - 1; i >= a; i--)
+#define elif else if
+ 
+bool isprime(ll n)
+{
+    if (n < 2) return false;
+    for (ll i = 2; i * i <= n; i++)
+        if (n % i == 0) return false;
+    return true;
+}
+ 
+ll pow(ll a, ll b) // O(log(b))
+{ 
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1) res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+void solve()
+{
+int n;
+    cin >> n;
+    ll ans = 0;
+    vector<ll> b(n);
+    for (int i = 0; i < n; i++) {
+        ll x;
+        cin >> x;
+        ans += x;
+    }
+        ll mx = 0;
+ 
+        for (int i = 0; i < n; i++) {
+            cin >> b[i];
+            ans += b[i];
+            mx = max(mx, b[i]);
+        }
+        ans -= mx;
+ 
+        cout << ans << '\n';
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    ll T; cin >> T;
+    while (T--)
+    solve();
+    return 0;
+}
