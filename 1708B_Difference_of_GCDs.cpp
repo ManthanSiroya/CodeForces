15498e3f80b630013285351fe56e94d6e70ad417
Manthan Siroya <237607330+ManthanSiroya@users.noreply.github.com>
2026-08-27 23:23:19 +0530

    Sync Codeforces: 1708B - Difference of GCDs [C++20 (GCC 13-64)]

---
+++ 1708B_Difference_of_GCDs.cpp
@@ -0,0 +1,62 @@
+// Winners never Quit!
+#include <bits/stdc++.h> // Problem - 
+using namespace std;
+ 
+#define ll long long
+#define pr pair<ll, ll>
+#define pb push_back
+#define ran(a) a.begin(), a.end()
+#define fr(i, a, n) for (ll i = a; i < n; i++)
+#define frr(i, a, n) for (ll i = n - 1; i >= a; i--)
+#define elif else if
+ 
+bool isprime(ll n)
+{
+    if (n < 2) return false;
+    for (ll i = 2; i * i <= n; i++)
+        if (n % i == 0) return false;
+    return true;
+}
+ 
+ll pow(ll a, ll b) // O(log(b))
+{ 
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1) res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+void solve()
+{
+    ll n,l,r;
+    cin>>n>>l>>r;
+    vector<ll> ans(n);
+    ll x=0;
+    frr(i,1,n+1){
+        x=r%i;
+        if(r-x<l || r<x){
+            cout<<"NO\n";
+            return;
+        }
+        elif(r>=x) {
+            ans[i-1]=r-x;
+        }
+    }
+    cout<<"YES\n";
+    fr(i,0,n) cout<<ans[i]<<" ";
+    cout << endl;
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    ll T; cin >> T;
+    while (T--)
+    solve();
+    return 0;
+}
