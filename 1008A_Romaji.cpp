44b7f2875bceeced84cd89bef8221e9430ff63e1
Manthan Siroya <manthansiroya31121@gmail.com>
2026-09-07 00:00:24 +0530

    Sync Codeforces: 1008A - Romaji [C++20 (GCC 13-64)]

---
+++ 1008A_Romaji.cpp
@@ -0,0 +1,72 @@
+// Winners never Quit!
+#include <bits/stdc++.h> // Problem -
+using namespace std;
+ 
+#define ll long long
+#define pr pair<ll, ll>
+#define pb push_back
+#define ran(a) a.begin(), a.end()
+#define fr(i, a, n) for (ll i = a; i < n; i++)
+#define frr(i, a, n) for (ll i = n - 1; i >= a; i--)
+#define elif else if
+ 
+bool isprime(ll n)
+{
+    if (n < 2)
+        return false;
+    for (ll i = 2; i * i <= n; i++)
+        if (n % i == 0)
+            return false;
+    return true;
+}
+ 
+ll pow(ll a, ll b) // O(log(b))
+{
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1)
+            res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+void solve()
+{
+    string s;
+    cin >> s;
+    fr(i, 0, s.size() - 1)
+    {
+        if (s[i] != 'a' && s[i] != 'e' && s[i] != 'i' &&
+            s[i] != 'o' && s[i] != 'u' && s[i] != 'n')
+        {
+            if (s[i + 1] != 'a' && s[i + 1] != 'e' &&
+                s[i + 1] != 'i' && s[i + 1] != 'o' &&
+                s[i + 1] != 'u')
+            {
+                cout << "NO\n";
+                return;
+            }
+        }
+    }
+    ll n = s.size() - 1;
+    if (s[n] != 'a' && s[n] != 'e' && s[n] != 'i' && s[n] != 'o' && s[n] != 'u' && s[n] != 'n')
+    {
+        cout << "NO\n";
+        return;
+    }
+    cout << "YES\n";
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    // ll T; cin >> T;
+    // while (T--)
+    solve();
+    return 0;
+}
