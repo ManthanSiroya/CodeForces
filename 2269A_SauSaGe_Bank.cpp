d0049fa159cbc6d4382b0495c9b8b29366ab3483
Manthan Siroya <manthansiroya31121@gmail.com>
2026-09-26 20:13:22 +0530

    Sync Codeforces: 2269A - SauSaGe Bank [C++20 (GCC 13-64)]

---
+++ 2269A_SauSaGe_Bank.cpp
@@ -0,0 +1,49 @@
+// Winners never Quit!
+#include <bits/stdc++.h> // Problem - 
+using namespace std;
+ 
+#define ll long long
+#define pr pair<ll, ll>
+#define pb push_back
+#define ran(a) a.begin(), a.end()
+#define fr(i, a, n) for (ll i = a; i < n; i++)
+#define frr(i, a, n) for (ll i = n - 1; i >= a; i--)
+#define elif else if
+ 
+bool isprime(ll n)
+{
+    if (n < 2) return false;
+    for (ll i = 2; i * i <= n; i++)
+        if (n % i == 0) return false;
+    return true;
+}
+ 
+ll pow(ll a, ll b) // O(log(b))
+{ 
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1) res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+void solve()
+{
+    ll n,m;
+    cin>>n>>m;
+    ll ans1=pow(2,(n-m+1));
+    cout<<((m-1)*2+ans1)<<endl;
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    ll T; cin >> T;
+    while (T--)
+    solve();
+    return 0;
+}
